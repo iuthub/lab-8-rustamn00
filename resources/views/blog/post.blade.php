039958f76ec0983c121c813604769aa126b69f36
asus <aaa@aaa.aaa>
2020-03-31 16:41:56 +0500

    first

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum laudantium, amet pariatur reprehenderit esse quas quaerat quae error itaque hic nam perspiciatis animi non deleniti dolores natus quasi totam asperiores!</p>
+        </div>
+    </div>
+@endsection
